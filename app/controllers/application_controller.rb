8da1c98ec84a66ea945649bffbacfb58e54a8f55
Aasemaaneh <aasemaaneh8@gmail.com>
2023-11-11 00:54:52 -0500

    Feature User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-
+  helper_method :current_user, :logged_in?
   private
 
   def cart
@@ -28,5 +28,18 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    redirect_to login_path unless logged_in?
   end
 end
